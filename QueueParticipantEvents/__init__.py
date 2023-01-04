44f58d15cb4a2cb8f5dce9c4642f7842dd574b68
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2023-01-04 15:11:59 -0500

    Remove multiple operator code

--- QueueParticipantEvents/__init__.py
+++ QueueParticipantEvents/__init__.py
@@ -3,11 +3,10 @@ import json
 import re
 
 import azure.functions as func
-import azure.durable_functions as df
 import cosmosdb_helpers as db_help
-from client import call_operators, end_api, get_env
+from client import call_operators, get_env
 
-async def main(msg: func.QueueMessage, APIOrchestrationClient: str) -> None:
+def main(msg: func.QueueMessage) -> None:
     events_db_name = get_env('EventsDatabaseName')
     activecalls_container_name = get_env('ActiveCallsContainerName')
     config_container_name = get_env('ConfigContainerName')
@@ -29,9 +28,7 @@ async def main(msg: func.QueueMessage, APIOrchestrationClient: str) -> None:
     
     if not id:
         return
-    
-    client = df.DurableOrchestrationClient(APIOrchestrationClient)
-    
+   
     if event_type == 'participant_connected':
         logging.info(f'QueueParticipantEvents.main: Event {id} is type {event_type}, sending to active calls db')
         db_help.db_add(db_events, event)
@@ -51,24 +48,24 @@ async def main(msg: func.QueueMessage, APIOrchestrationClient: str) -> None:
             call_info['operator'] = conf.get('operator', None)
             if conf and conf.get('operator') and event.get('data', {}).get('call_direction') == 'in':
                 logging.info(f'QueueParticipantEvents.main: Calling operators from {call_info.get("destination_alias", "")} to {call_info.get("conference", "")}')
-                await call_operators(call_info, client)
+                call_operators(call_info)
 
     elif event_type == 'participant_disconnected':
         logging.info(f'QueueParticipantEvents.main: Event {id} is type {event_type}, deleting from active calls db ')
         db_help.db_delete(db_events, event)
         
-    elif event_type == 'participant_updated':
-        if event.get('data', {}).get('has_media') or event.get('data', {}).get('service_type') != 'conference':
-            return
-        call_id = event.get('data', {}).get('call_id')
-        logging.info(f'QueueParticipantEvents.main: Participant update event received for : {event.get("data", {}).get("display_name")} calling {event.get("data", {}).get("conference")}')
+    # elif event_type == 'participant_updated':
+    #     if event.get('data', {}).get('has_media') or event.get('data', {}).get('service_type') != 'conference':
+    #         return
+    #     call_id = event.get('data', {}).get('call_id')
+    #     logging.info(f'QueueParticipantEvents.main: Participant update event received for : {event.get("data", {}).get("display_name")} calling {event.get("data", {}).get("conference")}')
 
-        if call_id:
-            apitokens_container_name = get_env('APITokenContainerName')
-            db_api = db_help.db_init(events_db_name, apitokens_container_name, '/operator')
+    #     if call_id:
+    #         apitokens_container_name = get_env('APITokenContainerName')
+    #         db_api = db_help.db_init(events_db_name, apitokens_container_name, '/operator')
             
-            query = f'SELECT * FROM {apitokens_container_name} c WHERE c.id = "{call_id}"'
-            api_call = db_help.db_query(db_api, query)
-            if api_call:
-                logging.info(f'QueueParticipantEvents.main: End API event called for : {event.get("data", {}).get("display_name")} calling {event.get("data", {}).get("conference")}')
-                end_api(call_id, db_api, client)
+    #         query = f'SELECT * FROM {apitokens_container_name} c WHERE c.id = "{call_id}"'
+    #         api_call = db_help.db_query(db_api, query)
+    #         if api_call:
+    #             logging.info(f'QueueParticipantEvents.main: End API event called for : {event.get("data", {}).get("display_name")} calling {event.get("data", {}).get("conference")}')
+    #             end_api(call_id, db_api, client)
--- client.py
+++ client.py
@@ -1,7 +1,5 @@
-from typing import Container, List
+from typing import List
 from xmlrpc.client import Boolean
-from threading import Timer
-from datetime import datetime, timedelta
 
 import requests
 import json
@@ -10,38 +8,7 @@ import os
 import validators
 import re
 import logging
-import azure.durable_functions as df
 
-
-# class api_client:
-#     def get_token(self, resp: requests.Response) -> None:
-#         if resp.status_code == 200:
-#             self.token = json.loads(resp.text)['result']['token']
-#             self.ref = Timer(40.0, self.refresh)
-#             self.ref.start()
-
-#     def __init__(self, fqdn: str, conf: str, pin: str, name: str = "API") -> None:
-#         self.url_base = f'{fqdn}/api/client/v2/conferences/{conf}'
-#         body = {"display_name": name}
-#         header = {"pin" : pin}
-#         resp = requests.post(f'{self.url_base}/request_token', json=body, headers=header)
-#         if resp.status_code == 200:
-#             self.get_token(resp)
-#             self.uuid = json.loads(resp.text)['result']['participant_uuid']
-#             self.conference = conf
-
-#     def __repr__(self) -> str:
-#         return self.uuid
-        
-#     def refresh(self) -> None:
-#         header = {"token" : self.token}
-#         resp = requests.post(f'{self.url_base}/refresh_token', headers=header)
-#         self.get_token(resp)
-
-#     def release(self) -> None:
-#         self.ref.cancel()
-#         header = {"token" : self.token}
-#         resp = requests.post(f'{self.url_base}/release_token', headers=header)
 
 def get_fqdn(fqdn: str) -> str:
     prefix = r"^(https://|http://)?(.+)"
@@ -84,10 +51,7 @@ def get_operator(oper: dict) -> List:
         return []
     
     # If all operators are to be returned, return the keys.  Else find the first operator with the fewest participants
-    if oper.get('dial_all', False):
-        operator = list(operators.keys())
-    else:
-        operator = [k for k, v in operators.items() if v==min(operators.values())][:1]
+    operator = [k for k, v in operators.items() if v==min(operators.values())][:1]
         
     return operator
 
@@ -115,66 +79,10 @@ def management_dial(from_alias: str, to_alias: str, display_name: str) -> None:
     
         requests.post(f'{fqdn}{api_dial}', auth=(uname, pwd), json=data)
         
-async def call_operators(call_info: dict, client: df.DurableOrchestrationClient) -> None:
+def call_operators(call_info: dict) -> None:
     operators = get_operator(call_info.get('operator'))
             
     if len(operators) == 1:
         management_dial(call_info.get('destination_alias'), operators[0], call_info.get('operator', {}).get('display_name'))
-
-    elif operators:
-        fqdn = get_fqdn(get_env('ConferenceNodeFQDN'))
-        if not fqdn:
-            logging.info(f'client.py.find_operator: Invalid value for ConferenceNodeFQDN')
-            return
-
-        events_db_name = get_env('EventsDatabaseName')
-        apitoken_container_name = get_env('APITokenContainerName')
-
-        # Initialize events database
-        db_api = db_help.db_init(events_db_name, apitoken_container_name, '/operator')
-        body = {'display_name': call_info.get('conference')}
-        header = {'pin' : ''}
-            
-        for operator in operators:
-            url_base = f'{fqdn}/api/client/v2/conferences/{operator}'
-            client_info = {'fqdn' : fqdn, 'caller' : call_info.get('destination_alias'), 'pin' : '', 'operator' : operator, 'display_name' : call_info.get('operator', {}).get('display_name')}
-
-            resp = requests.post(f'{url_base}/request_token', json=body, headers=header)
-            if resp.status_code == 200:
-                client_info['token'] = json.loads(resp.text).get('result', {}).get('token')
-                client_info['id'] = json.loads(resp.text).get('result', {}).get('participant_uuid')
-                client_info['next_update'] = datetime.strftime(datetime.now() + timedelta(seconds=40), '%Y-%m-%d %H:%M:%S.%f')
-                client_info['instance_id'] = await client.start_new('APIClientOrchestrator', None, client_info)
-                client.start_new()
-
-                db_help.db_add(db_api, client_info)        
-    return
-
-def end_api(call_id: str, db_api: Container, client: df.DurableOrchestrationClient) -> None:
-    logging.info(f'client.py.end_api: Deleting all API calls associated with call-id {call_id}')
-
-    fqdn = get_fqdn(get_env('ConferenceNodeFQDN'))
-    if not fqdn:
-        logging.info(f'client.py.find_operator: Invalid value for ConferenceNodeFQDN')
-        return
-
-    apitoken_container_name = get_env('APITokenContainerName')
-
-    # Initialize events database
-    api_calls = db_help.db_query(db_api, f'SELECT * FROM {apitoken_container_name} c WHERE c.id = "{call_id}"')
-    if api_calls:
-        this_call = api_calls[0]
-        operator = this_call.get('operator')
-        caller = this_call.get('caller')
-        management_dial(caller, operator, this_call.get('display_name', 'API'))
-        calls = db_help.db_query(db_api, f'SELECT * FROM {apitoken_container_name} c WHERE c.caller = "{caller}"')
-
-        for call in calls:
-            url_base = f'{fqdn}/api/client/v2/conferences/{call.get("operator")}'
-            header = {"token" : call.get('token')}
-            resp = requests.post(f'{url_base}/release_token', headers=header)
-            if resp.status_code == 200:
-                client.terminate(call.get('instance_id'), "Call ended")
-                db_help.db_delete(db_api, call)        
-            
+     
     return
